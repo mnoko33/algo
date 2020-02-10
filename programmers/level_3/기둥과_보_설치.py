9a599a61276f3e30f1a77de92ae4315b98cb68e5
sangtaek lee <srs99125@naver.com>
2020-02-11 00:01:39 +0900

    solving 기둥과 보 설치.py

--- programmers/level_3/기둥과_보_설치.py
+++ programmers/level_3/기둥과_보_설치.py
@@ -1,8 +1,9 @@
 def solution(n, build_frame):
-    wall = [[0] * (n+1) for _ in range(n+1)]
     result = []       
     for order in build_frame:
         x,y,a,b = order
+        print('========')
+        print(order)
         # 기둥 설치
         if a == 0 and b == 1:
             if y == 0 or [x, y-1, 0] in result or [x-1, y, 1] in result or [x, y, 1] in result:
@@ -13,20 +14,23 @@ def solution(n, build_frame):
                 result.append([x,y,1])
         # 기둥 제거
         elif a == 0 and b == 0:
-            if [x,y+1,1] in result and [x+1,y,0] not in result and [x+1,y+1,1] not in result:
-                continue
-            if [x-1,y+1,1] in result and [x-1,y,0] not in result and [x-2, y+1, 1] not in result:
-                continue 
-            if [x,y+1,0] in result and [x,y+1,1] not in result ans [x-1,y+1,1] not in result:
-                continue
-            result.remove([x,y,0])
+            if [x-1,y+1,1] in result and [x-1,y,0] in result:
+                result.remove([x,y,0])
+            elif [x,y+1,1] in result and [x+1,y,0] in result:
+                result.remove([x,y,0]) 
+            elif [x-1,y+1,1] in result and [x,y+1,1] in result and ([x-2,y+1,1] in result or [x-1,y,0] in result) and ([x+1,y+1,1] in result or [x+1,y,0] in result):
+                result.remove([x,y,0])
+            elif [x,y+1,0] not in result and [x-1,y+1,1] not in result and [x,y+1,1] not in result:
+                result.remove([x,y,0])
             
         # 보 제거
         elif a == 1 and b == 0:
-            if [x,y-1,0] not in result and [x+1,y-1,0] not in result and [x-1,y,1] in result and [x+1,y,1] in result:
-                continue
-            else:
+            if [x,y-1,0] in result or [x+1,y-1,0] in result:
                 result.remove([x,y,1])
+            elif [x+1,y,1] in result and [x-1,y,1] in result:
+                if [x+1,y-1,0] in result and [x-1,y-1,0] in result:
+                    result.remove([x,y,1])
+        print(result)        
     return sorted(result)
 
 n = 5
