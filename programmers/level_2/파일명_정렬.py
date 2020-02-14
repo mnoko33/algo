aff817a31583b54a5bc3c512bb0ecae5e1df278c
mnoko <mnoko92@gmail.com>
2020-02-14 17:56:33 +0900

    파일명 정렬

--- programmers/level_2/파일명_정렬.py
+++ programmers/level_2/파일명_정렬.py
@@ -6,44 +6,64 @@ def solution(files):
         is_head = False
         is_number = False
         for i,s in enumerate(file):
-            if is_head == False and type(s) is str:
+            if is_head == False and type(s) is str and s not in ['1','2','3','4','5','6','7','8','9','0']:
                 head += s
                 continue
-            else:
-                is_head = True
-                continue
-            
-            if is_number == True and type(s) is int:
+            is_head = True
+            if is_number == False and s in ['1','2','3','4','5','6','7','8','9','0']:
                 number += s
                 continue
-            else:
-                is_number = True
-                continue
-                
+            is_number = True
+
             if is_number is True and is_head is True:
                 tail = file[i:]
+                break
         return [head, number, tail]
                 
-    def compare(file1, file2):
-        
-        
-    def quick_sort(files):
-        s = 0
-        e = len(files) - 1
-        pivot = files[len(files) // 2]
-        while s < e:
+    def compare(file1, file2): # is file1 bigger than file2
+        [head1, number1, tail1] = file1
+        [head2, number2, tail2] = file2
+        if head1.upper() > head2.upper(): return 1
+        if head1.upper() < head2.upper(): return -1
+        if int(number1) > int(number2): return 1
+        if int(number1) < int(number2): return -1
+        return 0
+    
+    def quick_sort(files,s,e):
+        if e - s <= 1:
+            return
+        start = s
+        end = e
+        pivot = files[(s+e)//2]
+        while s <= e:
             while True:
-                if files[s] > pivot:
+                if compare(files[s], pivot) == 1:
                     break
                 s += 1
+                if s > end:
+                    break
+                
+
             while True:
-                if files[e] <= pivot:
+                if compare(pivot, files[e]) in [1, 0]:
                     break
                 e -= 1
-            if s < e:
+                if start > end:
+                    break
+                
+                
+            if s <= e:
                 files[s], files[e] = files[e], files[s]
-        quick_sort(files[:s+1])
-        quick_sort(files[s+1:])
-        
+                s += 1
+                e -= 1
+            
+        quick_sort(files, start, s-1)
+        quick_sort(files,s,end)
+    
     answer = []
+    files = [seperate(file) for file in files]
+    quick_sort(files,0,len(files)-1)
+    for file in files:
+        answer.append(file[0] + file[1] + file[2])    
+    
     return answer
